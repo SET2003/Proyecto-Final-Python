173516e49be4c969feefb9db36a1fef67769eb96
Torres Santiago <santiago_03torres@hotmail.com>
2024-12-08 11:37:54 -0300

    Agregue muchos archivos
    Agregue el excel y pdf de consignas, tambien meti en una carpeta "Archivos" donde van a estar archivos importantes, si ponemos fotos o cosas habria que meterlo ahi. Tambien hay que mantener el nombre "main.py" del archivo si o si. La carpeta "pages" tiene que tener ese nombre si o si y ahi son mas paginas secundarias por si queremos agregar, por ahora las dejo pero para probar, no se si las vamos a usar

---
+++ Archivos/main.py
@@ -0,0 +1,20 @@
+"""
+Barra lateral
+Pestañas
+Datos
+Graficos
+Hora interactiva con calendario
+
+"""
+import numpy as np
+import streamlit as st
+import pandas as pd
+
+"""
+# Calculo Generador Fotovoltaico
+
+
+"""
+
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
---
+++ Archivos/pages/Probando_Pagina_Otra.py
@@ -0,0 +1,6 @@
+import numpy as np
+import streamlit as st
+import pandas as pd
+
+st.markdown("# Page 2 ❄️")
+st.sidebar.markdown("# Page 2 ❄️")
